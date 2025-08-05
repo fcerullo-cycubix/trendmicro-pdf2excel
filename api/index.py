ddecc7d5a2b1f3c72938046d6cad48397b3a126f
Fabster <fcerullo@cycubix.com>
2025-08-05 11:58:16 -0300

    Connect Google Analytics with Measurement ID G-TTN61DXW69

--- api/index.py
+++ api/index.py
@@ -42,12 +42,12 @@ def upload_single_pdf():
   <script src="https://cdn.tailwindcss.com"></script>
   
   <!-- Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"></script>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-TTN61DXW69"></script>
   <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-    gtag('config', 'G-XXXXXXXXXX');
+    gtag('config', 'G-TTN61DXW69');
   </script>
   
   <style>
